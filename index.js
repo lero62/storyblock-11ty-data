554647f742abcb738ebd755d4cbcc9827e83f93d
Petro <83351038+lero62@users.noreply.github.com>
2023-07-12 09:35:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Node Modules
+const StoryblokTo11tyPlugin = require('./src/plugin/index')
+const StoryblokTo11tyData = require('./src/data')
+
+/**
+ * StoryblokTo11ty is the main class that fetches the data
+ * from Storyblok
+ */
+module.exports = {
+  importer: StoryblokTo11tyData,
+  plugin: StoryblokTo11tyPlugin
+}
